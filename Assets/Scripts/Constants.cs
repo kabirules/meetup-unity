89b6e944a739f966d26fa32461802e707cbf8377
kabirules <javifont@gmail.com>
2018-10-01 23:27:38 +0200

    constants file

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants {
+
+	public static string MEETUP_HOST = "https://api.meetup.com";
+
+	public static string METHOD_GROUPS = "/find/groups";
+  	public static METHOD_CITIES = "/2/cities";
+  	public static METHOD_TOPICS = "/find/topics";
+
+	public static string API_KEY = "";
+}
